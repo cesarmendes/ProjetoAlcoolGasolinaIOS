3eac790dcb8ec49cdb8eed206ccf8f5908a8a487
Cesar Mendes da Silva <cesarmendesdasilva@mac-mini.local>
2017-09-30 13:40:19 -0300

    Initial Commit

---
+++ AlcoolGasolina/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlcoolGasolina
+//
+//  Created by Cesar Mendes da Silva on 30/09/2017.
+//  Copyright © 2017 Cesar Mendes da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
