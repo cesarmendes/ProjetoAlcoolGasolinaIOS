f21ef2b6592160d522e1c88ec18ee2109045f49a
Cesar Mendes da Silva <cesarmendesdasilva@mac-mini.local>
2017-09-30 19:07:51 -0300

    Finalizacao do projeto

--- AlcoolGasolina/ViewController.swift
+++ AlcoolGasolina/ViewController.swift
@@ -10,6 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtGasolinaValor: UITextField!
+    @IBOutlet weak var txtAlcoolValor: UITextField!
+    @IBOutlet weak var lblResultado: UILabel!
+    
+    @IBAction func btnCalcular_Click(_ sender: Any) {
+        let strValorGasolina = txtGasolinaValor.text ?? ""
+        let strValorAlcool = txtAlcoolValor.text ?? ""
+        
+        if !strValorGasolina.isEmpty && !strValorAlcool.isEmpty
+        {
+            let decValorGasolina = Float(strValorGasolina)
+            let decValorAlcool = Float(strValorAlcool)
+            
+            if decValorAlcool != nil && decValorGasolina != nil
+            {
+                if (decValorAlcool! / decValorGasolina!) >= 0.7
+                {
+                    lblResultado.text = "Melhor usar Gasolina"
+                }
+                else
+                {
+                    lblResultado.text = "Melhor usar Alcool"
+                }
+            }
+            else
+            {
+               lblResultado.text = "Digite os preços primeiro!"
+            }
+        }
+        else
+        {
+            lblResultado.text = "Digite os preços primeiro!"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
